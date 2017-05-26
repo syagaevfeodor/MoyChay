83097d1f23324c95218e37cd4075a6584c724444
syagaevfeodor <syagaevfedya@gmail.com>
2017-05-26 10:08:33 +0300

    Commit message

---
+++ MoyChay/Functions.swift
@@ -0,0 +1,9 @@
+//
+//  Functions.swift
+//  MoyChay
+//
+//  Created by Федор on 26.05.17.
+//  Copyright © 2017 Федор. All rights reserved.
+//
+
+import Foundation
---
+++ favoriteTeaCell.swift
@@ -0,0 +1,17 @@
+//
+//  cellForFavoriteTea.swift
+//  MoyChay
+//
+//  Created by Федор on 26.05.17.
+//  Copyright © 2017 Федор. All rights reserved.
+//
+
+import Foundation
+
+class favoriteTeaCell: UITableViewCell {
+    
+    @IBOutlet weak var teaImage: UIImageView!
+    @IBOutlet weak var teaLabel: UILabel!
+    
+    
+}
